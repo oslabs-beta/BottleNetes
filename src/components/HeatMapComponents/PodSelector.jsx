ce723f0519107b4dcad49db41cbd774ef2a1c27c
Mark <mark.kietnghiem@gmail.com>
2024-12-28 00:06:32 -0800

    style: Fixed PodNameDisplay.jsx to mitigate the new lines when names get too long

--- src/components/HeatMapComponents/PodSelector.jsx
+++ src/components/HeatMapComponents/PodSelector.jsx
@@ -45,6 +45,7 @@ const PodSelector = ({
       <select
         id="pod-selector-dropdown"
         value={selectedValue} // This controls what's displayed in the closed select
+        className="w-full rounded-lg border-2 border-slate-200 bg-gradient-to-r from-slate-200 to-slate-100 px-3 py-2 text-center text-sm font-medium text-slate-500 transition duration-200 hover:brightness-90"
         onChange={(e) => {
           const value = e.target.value;
           setSelectedValue(value); // Update what's shown in the closed select
@@ -65,7 +66,6 @@ const PodSelector = ({
             }
           }
         }}
-        className="rounded-lg bg-gradient-to-r from-slate-200 to-slate-100 px-3 py-2 text-sm font-medium text-slate-500 border-2 border-slate-200 w-full transition duration-200 hover:brightness-90 text-center"
       >
         <option value="">Select a Pod</option>
         {Object.entries(podsByNamespace).map(([namespace, pods]) => (
