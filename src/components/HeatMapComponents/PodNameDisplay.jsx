9fe1e7a32b78391cad5540697d12fa5a0adb94e1
Mark <mark.kietnghiem@gmail.com>
2024-12-28 00:06:32 -0800

    style: Fixed PodNameDisplay.jsx to mitigate the new lines when names get too long

--- src/components/HeatMapComponents/PodNameDisplay.jsx
+++ src/components/HeatMapComponents/PodNameDisplay.jsx
@@ -9,49 +9,49 @@ import "../../Overview.css";
 const PodNameDisplay = ({ clickedPod, setClickedPod }) => {
   const { podName, namespace, deploymentName } = clickedPod;
 
-  // move setClickedPod iside an useEffect to avoid calling setClickedPod directly during render (warning in console).
+  // move setClickedPod inside an useEffect to avoid calling setClickedPod directly during render (warning in console).
   useEffect(() => {
     if (deploymentName === "deployment not found") {
       setClickedPod((prev) => ({
         ...prev,
-        deploymentName: "-",
+        deploymentName: "undefined",
       }));
     }
   }, [deploymentName, setClickedPod]);
 
   return (
-    <div className="w-full bg-gradient-to-r from-[#0f172a] to-[#1e40af] py-4 text-center text-[#e2e8f0] transition-all">
+    <div className="bg-gradient-to-r from-[#0f172a] to-[#1e40af] py-4 text-center text-[#e2e8f0]">
       <div
         id="overview-info"
-        className="mx-60 flex flex-1 items-center justify-between gap-28 transition-all duration-300"
+        className="mx-10 items-center justify-between gap-5 transition duration-300 grid grid-cols-4"
       >
         <div
           id="namespace"
-          className="flex-shrink flex-grow basis-1/4 rounded-xl border-4 p-4 transition-all duration-500"
+          className="rounded-xl border-4 p-4 transition duration-500 h-full"
         >
           <h2 className="text-2xl font-extrabold">Namespace</h2>
           <br />
-          <p className="text-4xl font-semibold">{podName ? namespace : "-"}</p>
+          <p className="text-3xl font-semibold">{podName ? namespace : "-"}</p>
         </div>
         <div
           id="pod-name"
-          className="flex-shrink flex-grow basis-1/4 rounded-xl border-4 p-4 transition-all duration-500"
+          className="rounded-xl border-4 p-4 transition duration-500 col-span-2 h-full"
         >
           <h2 className="text-2xl font-extrabold">
             {podName ? "Selected Pod" : "No Pod Selected"}
           </h2>
           <br />
-          <p className="text-4xl font-semibold">
+          <p className="text-3xl font-semibold">
             {podName ? podName : "Displaying Node Average Metrics"}
           </p>
         </div>
         <div
           id="deployment"
-          className="flex-shrink flex-grow basis-1/4 rounded-xl border-4 p-4 transition-all duration-500"
+          className="rounded-xl border-4 p-4 transition duration-500 h-full"
         >
           <h2 className="text-2xl font-extrabold">Deployment</h2>
           <br />
-          <p className="text-4xl font-semibold">
+          <p className="text-3xl font-semibold">
             {podName ? deploymentName : "-"}
           </p>
         </div>
